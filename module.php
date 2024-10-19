f11d4ee08b33774dc47f63a64baa8096006146ee
Tadhg Boyle <tadhgsmboyle@gmail.com>
2024-10-19 14:04:36 -0700

    wip

--- module.php
+++ module.php
@@ -5,14 +5,18 @@ use NamelessMC\Framework\Extend;
 return [
     (new Extend\Language(__DIR__ . '/language')),
     (new Extend\FrontendPages)
-        ->register('/members', 'members.members', \NamelessMC\Members\Pages\Members::class),
+        ->register('/', 'members/members', \NamelessMC\Members\Pages\Members::class),
     (new Extend\PanelPages)
-        ->register('/', 'members.member_lists', \NamelessMC\Members\Pages\Panel\Index::class)
-        ->register('/settings', 'members.members', \NamelessMC\Members\Pages\Panel\Settings::class),
+        ->register('/', 'members/member_lists', \NamelessMC\Members\Pages\Panel\Index::class)
+        ->register('/settings', 'members/members', \NamelessMC\Members\Pages\Panel\Settings::class),
     (new Extend\Queries)
         ->register('/member_list', \NamelessMC\Members\Queries\MemberList::class),
     (new Extend\Permissions)
-        ->register(['admincp.members' => 'members.member_lists']),
+        ->register([
+            'admin' => [
+                'admincp.members' => 'members/member_lists',
+            ],
+        ]),
     (new Extend\DebugInfo)
         ->register(\NamelessMC\Members\DebugInfo\Provider::class),
     (new Extend\Events)
