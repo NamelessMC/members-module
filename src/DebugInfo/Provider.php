a61c3eb650b45ed9bbaf5f16226cf3daf6eed82c
Tadhg Boyle <tadhgsmboyle@gmail.com>
2024-10-21 17:31:51 -0700

    wip

--- src/DebugInfo/Provider.php
+++ src/DebugInfo/Provider.php
@@ -2,9 +2,13 @@
 
 namespace NamelessMC\Members\DebugInfo;
 
+use NamelessMC\Framework\Debugging\DebugInfoProvider;
+
 class Provider extends DebugInfoProvider {
 
     public function provide(): array {
-        return [];
+        return [
+            'foo' => 'bar'
+        ];
     }
 }
