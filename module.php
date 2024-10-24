993b47a3fd811abf0842937d37cc5afb3a21cc00
Tadhg Boyle <tadhgsmboyle@gmail.com>
2024-10-23 21:50:14 -0700

    wip

--- module.php
+++ module.php
@@ -8,11 +8,9 @@ return [
     (new Extend\FrontendPages)
         // TODO allow registerPageRepository to make page registration class-based if needed.
         // IE: forms module is dynamically registering pages
-        ->templateDirectory(__DIR__ . '/views')
         // TODO, don't require defining page name here and in the class
         ->register('/members', 'members', 'members/members', \NamelessMC\Members\Pages\Members::class, true),
     (new Extend\PanelPages)
-        ->templateDirectory(__DIR__ . '/panel_views')
         // TODO, don't require defining page permission and name here and in the class
         ->register('/members/settings', 'members_settings', 'members/member_list_settings', \NamelessMC\Members\Pages\Panel\Settings::class, 'admincp.members', 'fas fa-cogs')
         ->register('/members', 'member_lists', 'members/member_lists', \NamelessMC\Members\Pages\Panel\Index::class, 'admincp.members', 'fas fa-users'),
